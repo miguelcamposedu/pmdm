4d79b70c00fe2daba440f59e5ead60bac35f5f49
Miguel Campos <camposmiguel@gmail.com>
2024-10-01 08:47:35 +0200

    selectores, clases y ready

---
+++ index.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  // Selector de etiqueta
+  $("h1").html("adios mundo cruel");
+
+  // Selector de ID (#)
+  $("#page-title").html("Titulo Hola Mundo");
+
+  // Selector de clase (.)
+  $(".red").attr("style", "color:#FF0000");
+
+  // Evento click
+  $(document).on("click", "#btn-clear", function () {
+    $("h1").html("");
+  });
+
+  $(document).on("click", "#btn-restore", function () {
+    // Selector de etiqueta
+    $("h1").html("adios mundo cruel");
+
+    // Selector de ID (#)
+    $("#page-title").html("Titulo Hola Mundo");
+  });
+
+  $(document).on("click", "#add-yellow", function () {
+    $("#page-title").addClass("yellow");
+  });
+
+  $(document).on("click", "#delete-yellow", function () {
+    $("#page-title").removeClass("yellow");
+  });
+});
