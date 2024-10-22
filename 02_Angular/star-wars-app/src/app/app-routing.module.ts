04d7d071ed00addf02f0f5362ead72028b80cf75
Miguel Campos <camposmiguel@gmail.com>
2024-10-22 10:37:16 +0200

    angular routing

--- 02_Angular/star-wars-app/src/app/app-routing.module.ts
+++ 02_Angular/star-wars-app/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VehiclesListComponent } from './components/vehicles-list/vehicles-list.component';
+import { PeopleListComponent } from './components/people-list/people-list.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'vehicles', component: VehiclesListComponent },
+  { path: 'people', component: PeopleListComponent },
+  { path: '', redirectTo: '/vehicles', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- 02_Angular/star-wars-app/src/app/app.module.ts
+++ 02_Angular/star-wars-app/src/app/app.module.ts
@@ -6,9 +6,12 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { VehiclesListComponent } from './components/vehicles-list/vehicles-list.component';
 import { provideHttpClient } from '@angular/common/http';
+import { PeopleListComponent } from './components/people-list/people-list.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, VehiclesListComponent],
+  declarations: [AppComponent, VehiclesListComponent, PeopleListComponent, MenuComponent, PageNotFoundComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent],
